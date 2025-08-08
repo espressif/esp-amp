a9a86a84611eb4ccfefe53b50d7f749e6ee1e2b3
lizhiang <lizhiang@espressif.com>
2025-08-08 15:18:55 +0800

    test(panic): add global panic flag
    we defined esp_amp_subcore_panic_handler() in panic_test. this panic
    handler is shared by all tests. global panic flag can be accessed by
    other tests and monitor subcore panic event.

--- test_apps/esp_amp_basic_tests/maincore/test_panic_main.c
+++ test_apps/esp_amp_basic_tests/maincore/test_panic_main.c
@@ -14,9 +14,6 @@
 #include "esp_amp.h"
 #include "esp_amp_panic.h"
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
-
 #include "unity.h"
 #include "unity_test_runner.h"
 
@@ -30,10 +27,11 @@
 extern const uint8_t subcore_test_panic_bin_start[] asm("_binary_subcore_test_panic_bin_start");
 extern const uint8_t subcore_test_panic_bin_end[]   asm("_binary_subcore_test_panic_bin_end");
 
-static bool is_subcore_abort = false;
-static bool is_subcore_assert = false;
+/* global test variables */
+bool g_test_is_subcore_abort = false;
+bool g_test_is_subcore_assert = false;
+bool g_test_is_subcore_panic = false;
 
-static SemaphoreHandle_t g_esp_amp_subcore_panic_sem = NULL;
 static uint32_t g_test_id_value = 0;
 
 static void subcore_test_panic_subcore_init(void)
@@ -42,9 +40,6 @@ static void subcore_test_panic_subcore_init(void)
 
     atomic_uint *test_bits = (atomic_uint *)esp_amp_sys_info_alloc(SYS_INFO_ID_TEST_BITS, sizeof(uint32_t));
     atomic_init(test_bits, g_test_id_value);
-
-    g_esp_amp_subcore_panic_sem = xSemaphoreCreateBinary();
-    TEST_ASSERT_NOT_EQUAL(NULL, g_esp_amp_subcore_panic_sem);
 
     TEST_ASSERT_EQUAL(ESP_OK, esp_amp_load_sub(subcore_test_panic_bin_start));
     TEST_ASSERT_EQUAL(ESP_OK, esp_amp_start_subcore());
@@ -58,26 +53,38 @@ void esp_amp_subcore_panic_handler(void)
     esp_amp_subcore_panic_dump_t *panic_dump = (esp_amp_subcore_panic_dump_t *)g_esp_amp_subcore_panic_dump;
     if (panic_dump->is_abort == 1) {
         if (strncmp(panic_dump->abort_details, SUBCORE_ABORT_STR, strlen(SUBCORE_ABORT_STR)) == 0) {
-            is_subcore_abort = true;
+            g_test_is_subcore_abort = true;
         } else if (strncmp(panic_dump->abort_details, SUBCORE_ASSERT_STR, strlen(SUBCORE_ASSERT_STR)) == 0) {
-            is_subcore_assert = true;
+            g_test_is_subcore_assert = true;
         }
     }
-    xSemaphoreGive(g_esp_amp_subcore_panic_sem);
+
+    extern void esp_amp_subcore_panic_handler_default(void);
+    esp_amp_subcore_panic_handler_default();
+
+    g_test_is_subcore_panic = true;
 }
 
 TEST_CASE("test abort on subcore", "[esp_amp]")
 {
     g_test_id_value = TEST_ID_ABORT;
     subcore_test_panic_subcore_init();
-    xSemaphoreTake(g_esp_amp_subcore_panic_sem, pdMS_TO_TICKS(2000));
-    TEST_ASSERT_EQUAL(true, is_subcore_abort);
+    int timeout = 2000;
+    while (g_test_is_subcore_panic == false && timeout > 0) {
+        vTaskDelay(pdMS_TO_TICKS(100));
+        timeout -= 100;
+    }
+    TEST_ASSERT_EQUAL(true, g_test_is_subcore_abort);
 }
 
 TEST_CASE("test assert on subcore", "[esp_amp]")
 {
     g_test_id_value = TEST_ID_ASSERT;
     subcore_test_panic_subcore_init();
-    xSemaphoreTake(g_esp_amp_subcore_panic_sem, pdMS_TO_TICKS(2000));
-    TEST_ASSERT_EQUAL(true, is_subcore_assert);
+    int timeout = 2000;
+    while (g_test_is_subcore_panic == false && timeout > 0) {
+        vTaskDelay(pdMS_TO_TICKS(100));
+        timeout -= 100;
+    }
+    TEST_ASSERT_EQUAL(true, g_test_is_subcore_assert);
 }
